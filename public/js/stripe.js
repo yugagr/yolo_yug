5f2ef3aa3bedb3ae0133c7947437cdc3d85e3f31
yugagr <yuga8401@gmail.com>
2025-07-30 11:33:21 +0530

    changeprod

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -6,9 +6,7 @@ const stripe = Stripe('pk_test_BUkd0ZXAj6m0q0jMyRgBxNns00PPtgvjjr');
 export const bookTour = async tourId => {
   try {
     // 1) Get checkout session from API
-    const session = await axios(
-      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
-    );
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
     console.log(session);
 
     // 2) Create checkout form + chanre credit card
